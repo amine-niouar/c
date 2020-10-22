ff369e2e096c235fbffa7f51679999c35f001123
Amine Niouar <niouar.amin@gmail.com>
2020-10-22 09:56:02 +0100

    comb 2

--- day03/ex06/ft_print_comb2.c
+++ day03/ex06/ft_print_comb2.c
@@ -4,9 +4,10 @@
 
 void ft_print_comb2()
 {
-    char space,coma;
+    char space,coma,newline;
     coma = ',';
     space = ' ';
+    newline = '\n';
     char a;
     a = '0';
     while(a <= '9')
@@ -29,7 +30,10 @@ void ft_print_comb2()
 	       write(1,&space,1);
 	       write(1,&c,1);
                write(1,&d,1);
-               write(1,&coma,1);	  
+	       if(a == '9' && b == '8' && c == '9' && d == '9')
+		   write(1,&newline,1);
+	       else    
+                   write(1,&coma,1);	  
                d++;
             }          
             c++;
@@ -40,8 +44,3 @@ void ft_print_comb2()
     }
 }
 
-int main(void)
-{
-  ft_print_comb2();
-  return 0;
-}
