6c7bd068c982684871952c0dc36401661a9f13f1
Amine Niouar <niouar.amin@gmail.com>
2020-11-10 17:40:43 +0100

    DAY3

--- day09/ex00/ft_strcmp.c
+++ day09/ex00/ft_strcmp.c
@@ -4,7 +4,7 @@ int ft_strcmp(char *s1,char *s2)
    int i,r;
    i = 0;
    r = 0;
-   while(s1[i] != '\0')
+   while(s1[i] != '\0' || s2[i] != '\0')
    {
       if( (s1[i] < s2[i]) || ( s1[i] > s2[i] ))
          return (s1[i] - s2[i]);       
